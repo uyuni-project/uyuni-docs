016ea654e7bdc40bd26bdca4421af591afa5e43e
mbussolotto <michele.bussolotto@suse.com>
2022-07-27 16:01:12 +0200

    fix ifeval

--- ifeval_routine.sh
+++ ifeval_routine.sh
@@ -5,10 +5,9 @@ usage() {
 cat << EOF  
 Usage: ./ifeval_routine [-cfh]
 Every ifeval tag requires above a blank line, otherwise translations will be corrupted.
-This tool is used for checking of fixing all adoc files
+This tool is used for checking all adoc files
 -h, --help                  Display help
 -c, --check                 Check all adoc files. Return number of lines where there's an error
--f, --fix                   Fix all adoc files.
 EOF
 # EOF is found above and hence cat command stops reading. This is equivalent to echo but much neater when printing out.
 }
@@ -35,9 +34,6 @@ case $1 in
 -c|--check) 
     export CHECK=1
     ;;
--f|--fix)
-    export FIX=1
-    ;;
 --)
     shift
     break;;
@@ -46,16 +42,11 @@ shift
 done
 
 if [ $CHECK == 1 ]; then
-    IF_EVAL_CORRECT=$(find -name "*\.adoc" -type f -print0 | xargs -0 grep -nr "^\s*$*ifeval" | wc -l)
-    IF_EVAL=$(find -name "*\.adoc" -type f -print0 | xargs -0 grep -nr "ifeval" | wc -l)
+    IF_EVAL_CORRECT=$(find -name "*\.adoc" -type f -print0 | xargs -0 grep -hrB1 'ifeval' | grep '^[[:blank:]]*$'| wc -l)
+    IF_EVAL=$(find -name "*\.adoc" -type f -print0 | xargs -0 grep -nr "^ifeval" | wc -l)
     RES=$(($IF_EVAL-$IF_EVAL_CORRECT))
     echo "Total number of errors: $RES"
     exit $RES
-fi
-
-if [ $FIX == 1 ]; then
-    find -name "*\.adoc" -type f -print0 | xargs -0 sed -i 's/^$ifeval::/\nifeval::/g'
-    exit 0
 fi
 
 exit 0
