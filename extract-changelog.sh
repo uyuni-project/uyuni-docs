3fa63bf4b6665caf518ac8b580564c0c18f61f83
Jcayouette <joe.cayouette@gmail.com>
2023-12-13 21:45:50 +0100

    testing script

--- extract-changelog.sh
+++ extract-changelog.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# File name of the changelog
-CHANGELOG_FILE=".changelog"
+# Path to the file containing the release notes
+FILE_PATH=".changelog"
 
-# Extracts the section of the changelog from the start of the file 
-# to the first occurrence of a dashed line.
-extract_latest_changelog() {
-    # Use awk to extract the text between two patterns
-    # Here, it extracts text from the beginning of the file to the first dashed line
-    awk '/^---$/{exit} {print}' $CHANGELOG_FILE
+# Function to extract release notes
+extract_release_notes() {
+    # Use awk to extract the text between two dashed lines
+    awk '/^---+$/{flag=!flag;next} flag' "$FILE_PATH"
 }
 
 # Call the function and store the result
-LATEST_CHANGELOG=$(extract_latest_changelog)
+RELEASE_NOTES=$(extract_release_notes)
 
 # Output the result
-echo "$LATEST_CHANGELOG"
+echo "$RELEASE_NOTES"
