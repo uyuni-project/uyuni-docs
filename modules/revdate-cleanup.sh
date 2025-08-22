184b7631eda1f2a0c1d8bcf47fe1dd2bf8b52597
jcayouette <jcayouette@suse.com>
2025-08-22 12:28:39 +0200

    updated revdate for 4.3

---
+++ modules/revdate-cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Find all .adoc files excluding Antora nav files (nav-*.adoc)
+find . -type f -name "*.adoc" ! -name "nav-*.adoc" | while read -r ADOC; do
+  # Remove any lines with :revdate: or :page-revdate: (anywhere in the file)
+  sed -i '/^:revdate:/d;/^:page-revdate:/d' "$ADOC"
+  echo "Removed revdate and page-revdate from $ADOC"
+done
+
