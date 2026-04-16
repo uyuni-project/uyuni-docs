f1f46aad26d7a707c34f90f3e9ba84f3072d620e
jcayouette <jcayouette@suse.com>
2026-04-16 08:44:27 +0200

    update

---
+++ branding/supplemental-ui/mlm/susecom-2025/js/qualatrix.js
@@ -0,0 +1,4 @@
+(function(){var g=function(g){
+this.go=function(){var a=document.createElement("script");a.type="text/javascript";a.src=g;document.body&&document.body.appendChild(a)};
+this.start=function(){var t=this;"complete"!==document.readyState?window.addEventListener?window.addEventListener("load",function(){t.go()},!1):window.attachEvent&&window.attachEvent("onload",function(){t.go()}):t.go()};};
+try{(new g("https://zn8qzumklkybbqaye-suselinux.siteintercept.qualtrics.com/SIE/?Q_ZID=ZN_8qZUmklKYbBqAYe")).start()}catch(i){}})();
